689a31fdd19be6af7222bd0390800344d38da138
kveita <kveitanordland@gmail.com>
2026-09-11 08:13:20 +0000

    fix: align timeout handling in request function with requestBuffer

--- api/_lib.js
+++ api/_lib.js
@@ -55,7 +55,7 @@ function request(url, options, callback) {
     res.on('aborted', function () { done(new Error('Request aborted.')); });
   });
   req.on('error', function (err) { done(err); });
-  req.setTimeout(45000, function () { req.destroy(); });
+  req.setTimeout(45000, function () { done(new Error('Upstream request timed out.')); });
   if (options.body) req.write(options.body);
   req.end();
 }
