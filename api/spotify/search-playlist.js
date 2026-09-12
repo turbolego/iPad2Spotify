e39cd5b6054e7b1a2e222bdec9d63489c0006400
kveita <kveitanordland@gmail.com>
2026-09-12 05:43:44 +0000

    fix: update search URL format to match Spotify documentation

--- api/spotify/search-playlist.js
+++ api/spotify/search-playlist.js
@@ -13,7 +13,7 @@ module.exports = function (req, res) {
       var cfg = lib.config(req);
       lib.spotifyToken(cfg, 'grant_type=refresh_token&refresh_token=' + encodeURIComponent(session.refresh_token), function (tokenErr, tokenStatus, token) {
         if (tokenErr || tokenStatus !== 200 || !token.access_token) return lib.json(res, 401, { error: 'Spotify authorization expired. Pair again.' });
-        var url = 'https://api.spotify.com/v1/search?type=playlist&limit=5&market=NO&q=' + encodeURIComponent(q);
+        var url = 'https://api.spotify.com/v1/search?q=' + encodeURIComponent(q) + '&type=playlist&market=NO&limit=10&offset=0';
         console.log('Starting playlist search request: ' + url);
         lib.request(url, { headers: { Authorization: 'Bearer ' + token.access_token } }, function (apiErr, status, data) {
           console.log('Playlist search request completed with status: ' + status);
